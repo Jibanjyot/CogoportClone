a9c4f4436ec207e09e2f4ad4e0eabb5bb40721f8
Jibanjyot <jibanjyotikalita03@gmail.com>
2023-07-08 20:58:25 +0530

    how is works

--- src/components/GetStarted2/GetStarted2.js
+++ src/components/GetStarted2/GetStarted2.js
@@ -11,7 +11,7 @@ const GetStarted2 = () => {
                     <a href="https://app.cogoport.com/en-IN/signup" target="_blank" class="get-started-2-button" rel="noreferrer">Get Started</a>
                 </div>
                 <div className='get-started-2-image-container'>
-                <img alt="productCheck" loading="lazy" width="280" height="360" decoding="async" data-nimg="1" class="styles_about_image__p8EBE" srcset="https://www.cogoport.com/_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Ftrade-tech-home-1.webp&amp;w=384&amp;q=75 1x, https://www.cogoport.com/_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Ftrade-tech-home-1.webp&amp;w=640&amp;q=75 2x" src="https://www.cogoport.com/_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Ftrade-tech-home-1.webp&amp;w=640&amp;q=75"/>
+                <img alt="productCheck" loading="lazy" width="280" height="360" decoding="async" data-nimg="1" className="image-style-2" srcset="https://www.cogoport.com/_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Ftrade-tech-home-1.webp&amp;w=384&amp;q=75 1x, https://www.cogoport.com/_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Ftrade-tech-home-1.webp&amp;w=640&amp;q=75 2x" src="https://www.cogoport.com/_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Ftrade-tech-home-1.webp&amp;w=640&amp;q=75"/>
                 </div>
 
             </div>
